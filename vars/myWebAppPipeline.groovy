e1ced7963c776b871302aeeff3d5ca1e7fa7021b
Joaquin Mendez <joaquinf2005@gmail.com>
2025-11-02 17:47:51 -0800

    removing docker install

--- vars/myWebAppPipeline.groovy
+++ vars/myWebAppPipeline.groovy
@@ -16,17 +16,7 @@ def call(Map config = [:]) {
                         def dockerExists = sh(script: 'which docker', returnStatus: true) == 0
                         
                         if (!dockerExists) {
-                            echo "📦 Installing Docker CLI..."
-                            sh '''
-                                whoami
-                                apt-get update
-                                apt-get install -y ca-certificates curl gnupg lsb-release
-                                mkdir -p /etc/apt/keyrings
-                                curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-                                echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-                                apt-get update
-                                apt-get install -y docker-ce-cli curl
-                            '''
+                           echo "📦 Docker not installed"
                         }
                         
                         // Verify Docker is working
